e9c94bccd97f9839d2802462823c1e6ae0fe0d93
Julian Anderson <julian@myboost.id>
2018-12-18 13:52:14 +0700

    finish footer

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -1,10 +1,16 @@
 import React from 'react';
-import { OuterContainer, Button, H2 } from './style';
+import { OuterContainer, Button, H2, Ul, Li } from './style';
 
 const Footer = () => (
   <OuterContainer>
     <H2>LET&apos;S GET IN TOUCH!</H2>
     <Button href="mailto:josephinethalia1412@gmail.com">email me</Button>
+    <Ul>
+      <Li><a href="https://instagram.com/josephinethallia">Instagram</a></Li>
+      <Li><a href="">Behance</a></Li>
+      <Li><a href="">Dribbble</a></Li>
+      <Li><a href="">Twitter</a></Li>
+    </Ul>
   </OuterContainer>
 )
 
--- src/components/Footer/style.js
+++ src/components/Footer/style.js
@@ -17,10 +17,30 @@ padding : 1% 4%;
 border-radius : 30px;
 color : ${ BLUE };
 justify-content : center;
+text-decoration : none;
 `;
 
 export const Ul = styled.ul`
 display : inline-block;
+padding : 0 0;
+padding-top : 3%;
+width : 100%;
+justify-content : center;
+align-items : center;
+text-align: center;
+`;
+
+export const Li = styled.li`
+display : inline-flex;
+align-items:center;
+justify-content:center;
+text-align: center;
+width : 10%;
+list-style : none;
+   a {
+     text-decoration : none;
+      color : #FFF;
+   }
 `;
 
 export const H2 = styled.h2`
