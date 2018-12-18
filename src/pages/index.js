2e0caf973af6511de1249e43955ea06e43f620a4
Julian Anderson <julian@myboost.id>
2018-12-18 09:50:15 +0700

    add react helmet

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import { Link } from 'gatsby'
+import { Helmet } from 'react-helmet';
 
 import Layout from '../components/layout'
 import Image from '../components/image'
@@ -7,6 +8,11 @@ import SEO from '../components/seo'
 
 const IndexPage = () => (
   <Layout>
+    <Helmet>
+      <meta charSet="utf-8" />
+      <title>Josephine Thallia - Graphic Designer</title>
+      <link href="https://fonts.googleapis.com/css?family=Fjalla+One|Open+Sans:300,400,500,600,700,800,900 " rel="stylesheet" />
+    </Helmet>
     <SEO title="Home" keywords={['gatsby', 'application', 'react']} />
     <h1>Hi people</h1>
     <p>Welcome to your new Gatsby site.</p>
